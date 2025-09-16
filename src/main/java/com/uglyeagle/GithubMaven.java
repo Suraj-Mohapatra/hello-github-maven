261cffb47b37b81e014e3377f3f11e77946aad1c
Suraj Mohapatra <57671417+Suraj-Mohapatra@users.noreply.github.com>
2025-09-16 16:07:15 +0530

    Add files via upload

---
+++ src/main/java/com/uglyeagle/GithubMaven.java
@@ -0,0 +1,13 @@
+
+package com.uglyeagle;
+
+/**
+ *
+ * @author UglyEagle
+ */
+public class GithubMaven {
+
+    public void greet() {
+        System.out.println("Hello! from Github Maven");
+    }
+}
